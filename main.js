b324a1b101ad00eff99ae87164fd8aaf63ecd58b
zadeq handal <zadeqhandal12@gmail.com>
2022-09-08 12:27:00 -0500

    se agrego funcionalidades java hexa

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const colors = ["green", "pink" ,"red", "rgba(133, 122, 200)", "#f15025"];
+const colors = ["green", "purple","blue","pink" ,"red", "rgba(133, 122, 200)", "#f15025", "#49577B"];
  
 const btn = document.getElementById('btn');
 const  color  = document.querySelector(".color");
